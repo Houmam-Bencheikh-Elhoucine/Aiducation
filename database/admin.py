97dce866b73f214c641a1f9b18e8cdd5f3000429
Houmam-Bencheikh-Elhoucine <72684999+Houmam-Bencheikh-Elhoucine@users.noreply.github.com>
2023-01-30 23:49:56 +0100

    Delete database directory

--- database/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Users,Announcements, Messages
-
-
-admin.site.register(Users)
-admin.site.register(Announcements)
-admin.site.register(Messages)
